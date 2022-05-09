6f2db82d4a1cb50617876d4613b99cda367a578d
vishal-ahirwar <vishlchoudhry0091@gmail.com>
2022-05-09 18:39:45 +0530

    stl

---
+++ src/main.cc
@@ -0,0 +1,14 @@
+
+
+//Auto Genrated C++ file by newton CLI
+#include<iostream>
+static inline void say_hello()
+{
+    std::cerr<<"Hello, World\n";
+};
+int main(int argc,char*argv[])
+{
+    say_hello();
+    return 0;
+};
+
